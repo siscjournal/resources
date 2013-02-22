cd6584f8246df1ba10a5785ad9071dc89cb15bdb
Hans Petter Langtangen <hpl@simula.no>
2013-02-22 09:37:25 +0100

    updates

--- doc/src/reviewing_time/make.sh
+++ doc/src/reviewing_time/make.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-if [ ! -d js ]; then
-  cp -r ../template/javascripts ../template/stylesheets .
-fi
+cp -r ../template/javascripts ../template/stylesheets .
+
 doconce format html SISC_editorial_procedures.do.txt --html-template=../template/siscjournal_template.html
 
 # Publish
-#cp SISC_editorial_procedures.html ../../web/pub
+cp SISC_editorial_procedures.html ../../web/pub
